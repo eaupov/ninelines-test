425b558a5cb2d7e52f35868c99298369d8abaeff
Emil <eaupov@gmail.com>
2021-11-02 21:21:09 +0500

    Верстка готова на 90%

---
+++ src/js/main.js
@@ -0,0 +1,19 @@
+import './vendor';
+import './helpers';
+import './components/social';
+import {ieFix} from './vendor/ie-fix';
+import {vhFix} from './vendor/vh-fix';
+import {actualYear} from './modules/actualYear';
+import header from './components/header';
+import lazyLoading from './modules/lazyLoading';
+import scrollToAnchor from './modules/scrollToAnchor';
+import backToTop from './modules/backToTop';
+
+ieFix();
+vhFix();
+actualYear();
+scrollToAnchor.init();
+
+header.init();
+lazyLoading.init();
+backToTop.init();
