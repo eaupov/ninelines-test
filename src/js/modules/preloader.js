2b04492afce91bf6c037714c03c71aabe3872cfa
Emil <eaupov@gmail.com>
2021-11-03 17:33:42 +0500

    Папка build

--- src/js/modules/preloader.js
+++ src/js/modules/preloader.js
@@ -1,6 +1,6 @@
 import Preloader from 'ninelines-preloader';
 
-Preloader.init('/images/preloader.svg')
+Preloader.init('./images/preloader.svg')
     .then(() => {
         document.querySelector('.preloader').style.display = 'none';
         $(window).on('load', showPage());
